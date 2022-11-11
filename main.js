8e546fc0d55b10b1a5da27ef121f75cbcf964c94
Spong3ibob <leonardomaran17@gmail.com>
2022-11-11 15:48:37 +0100

    aggiunto bonus

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ createApp({
   data() {
     return {
       message: 'Hello Word!',
-      picture: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.payplug.com%2Fit%2Fblog%2F10-migliori-siti-scaricare-immagini-gratis-e-store&psig=AOvVaw1uMvhdCv2Bu3rpvsqjB6er&ust=1668261911865000&source=images&cd=vfe&ved=0CA0QjRxqFwoTCLj2rIimpvsCFQAAAAAdAAAAABAD"
+      picture: "https://www.payplug.com/hubfs/Banque%20images_featured.png"
     }
   }
 }).mount('#app')
